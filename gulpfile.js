acbada59649562eb2e144c1d77f182f50dc83b16
vindelorme <43699138+vindelorme@users.noreply.github.com>
2021-01-12 12:26:49 +0900

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-var sourcemaps = require("gulp-sourcemaps");
-var uglify = require("gulp-uglify-es").default;
-var minifyCSS = require("gulp-csso");
-var clean = require("gulp-clean");
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify-es").default;
+const minifyCSS = require("gulp-csso");
+const clean = require("gulp-clean");
+const zip = require("gulp-zip");
+const rename = require("gulp-rename");
+const mergeStream = require("merge-stream");
 
 var paths = {
 	src: "src/",
 	dest: "./dist",
 }
 gulp.task("clean", function(cb) {
-	return gulp.src("dist", {read: false})
+	return gulp.src("dist/*", {read: false})
 	.pipe(clean());
 });
 gulp.task("build", function(cb) {
-	let apps = ["editor", "plotter", "helper", "grapher", "shared", "analyzer"];
+	//let apps = ["editor", "plotter", "helper", "grapher", "shared", "analyzer"];
+	let apps = ["editor", "shared", "analyzer"];
 	apps.forEach(function(a) { //Loop the array of apps to build
 		//Build the js files
 		gulp.src(paths.src + a + "/**/*.js")
@@ -24,13 +28,31 @@ gulp.task("build", function(cb) {
 		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest(paths.dest));
 		//Build the css files
-		gulp.src(paths.src + a + "/**/*css")
+		gulp.src(paths.src + a + "/**/*.css")
 		.pipe(sourcemaps.init())
 		.pipe(concat(a + "-styles.css"))
 		.pipe(minifyCSS())
 		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest(paths.dest));
 	});
+	cb();
+});
+gulp.task("release", function(cb) {
+	let folders = ["dependencies", "dist", "images"];
+	let stream = mergeStream();
+	folders.forEach(function(f) {
+		stream.add(
+			gulp.src(f + "/*")
+			.pipe(rename(function(file) {
+				file.dirname = f + '/' + file.dirname;
+			}))
+		);
+	});
+	stream.add(
+		gulp.src("Editor.html")
+	);
+	stream.pipe(zip("Release.zip"))
+	.pipe(gulp.dest(paths.dest));
 	cb();
 });
 gulp.task("default", gulp.series("clean", "build"));
